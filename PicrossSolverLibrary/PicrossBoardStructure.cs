b18fe9f44841e18dd263a860b1ea8d3d9f942dca
rzippo <raffaele.zippo@live.it>
2018-04-09 23:26:49 +0200

    Implement Board ctor from Puzzle

--- PicrossSolverLibrary/PicrossBoardStructure.cs
+++ PicrossSolverLibrary/PicrossBoardStructure.cs
@@ -1,16 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace PicrossSolverLibrary
 {
     public partial class PicrossBoard
     {
-        public bool[][] Matrix { get; }
+        public PicrossCellState[,] Matrix { get; }
         public PicrossLineRule[] ColumnRules { get; }
         public PicrossLineRule[] RowRules { get; }
 
-        public PicrossBoard(string json)
+        public int RowCount { get; }
+        public int ColumnCount { get; }
+
+        public PicrossBoard(PicrossPuzzle puzzle)
         {
-            //todo: implement PicrossBoard parser from json
+            RowCount = puzzle.RowCount;
+            ColumnCount = puzzle.ColumnCount;
+            Matrix = new PicrossCellState[RowCount, ColumnCount];
+
+            ColumnRules = new PicrossLineRule[ColumnCount];
+            RowRules = new PicrossLineRule[RowCount];
+
+            for (int columnIndex = 0; columnIndex < ColumnCount; columnIndex++)
+            {
+                ColumnRules[columnIndex] =
+                    new PicrossLineRule(puzzle.ColumnRules[columnIndex], RowCount);
+            }
+
+            for (int rowIndex = 0; rowIndex < RowCount; rowIndex++)
+            {
+                RowRules[rowIndex] =
+                    new PicrossLineRule(puzzle.RowRules[rowIndex], ColumnCount);
+            }
         }
     }
 }
---
+++ PicrossSolverLibrary/PicrossCellState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PicrossSolverLibrary
+{
+    public enum PicrossCellState
+    {
+        Void,
+        Filled
+    }
+}
