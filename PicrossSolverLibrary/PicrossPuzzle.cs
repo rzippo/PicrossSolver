97e760f57bcb89e0a3096e78dcad39c97460fc12
rzippo <raffaele.zippo@live.it>
2018-04-09 23:24:30 +0200

    Change json structure, add PicrossPuzzle class for deserialization

---
+++ PicrossSolverLibrary/PicrossPuzzle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace PicrossSolverLibrary
+{
+    public class PicrossPuzzle
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("column-count")]
+        public int ColumnCount { get; set; }
+
+        [JsonProperty("row-count")]
+        public int RowCount { get; set; }
+
+        [JsonProperty("column-rules")]
+        public IList<IList<int>> ColumnRules { get; set; }
+
+        [JsonProperty("row-rules")]
+        public IList<IList<int>> RowRules { get; set; }
+    }
+}
