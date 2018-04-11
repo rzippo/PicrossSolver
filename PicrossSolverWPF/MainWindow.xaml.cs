dc6b83f24f6f0a771a9655df2c263fc2c57a5473
rzippo <raffaele.zippo@live.it>
2018-04-11 11:49:49 +0200

    Add WPF project, retarget library to .NET Standard

---
+++ PicrossSolverWPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using PicrossSolverLibrary;
+
+
+namespace PicrossSolverWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PicrossBoard PicrossBoard { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void JsonPathSelector_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Load_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
