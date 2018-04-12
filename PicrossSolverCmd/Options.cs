18f987604b1a9ac6c9b63a2511059be3005b8f63
rzippo <raffaele.zippo@live.it>
2018-04-12 10:34:08 +0200

    Implement cmd application

---
+++ PicrossSolverCmd/Options.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace PicrossSolverCmd
+{
+    class Options
+    {
+        [Option('f', "file", Required = true, HelpText = "Json file to read the puzzle from")]
+        public string JsonPath { get; set; }
+
+        [Option(Default = false, HelpText = "Prints the board at each fast-forward step")]
+        public bool Verbose { get; set; }
+
+        //anything else?
+    }
+}
--- PicrossSolverCmd/Program.cs
+++ PicrossSolverCmd/Program.cs
@@ -1,12 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using CommandLine;
+using log4net;
+using log4net.Config;
+using Newtonsoft.Json;
+using PicrossSolverCmd;
+using PicrossSolverLibrary;
 
 namespace PicrossSolver
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+
+        public static PicrossBoard Board { get; set; }
+        public static PicrossPuzzle Puzzle { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConfigureLog4Net();
+           
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts))
+                .WithNotParsed<Options>((errs) => HandleParseError(errs));
+        }
+
+        private static void ConfigureLog4Net()
+        {
+            XmlDocument log4netConfig = new XmlDocument();
+            log4netConfig.Load(File.OpenRead("log4net.config"));
+            var logRepository = LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
+            log4net.Config.XmlConfigurator.Configure(logRepository, log4netConfig["log4net"]);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+            Console.WriteLine("Lazy error line: bad arguments");
+            PressEnterToContinue();
+        }
+
+        private static void RunOptionsAndReturnExitCode(Options opts)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(opts.JsonPath, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string json = sr.ReadToEnd();
+                        Puzzle = JsonConvert.DeserializeObject<PicrossPuzzle>(json);
+                        Console.WriteLine($"Loaded puzzle from {fs.Name}");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error while opening json");
+            }
+
+            Board = new PicrossBoard(Puzzle);
+            Console.WriteLine("Board loaded, next is solving...");
+            if (opts.Verbose)
+            {
+                Board.DebugSolve();
+            }
+            else
+            {
+                Board.Solve();
+            }
+            Console.WriteLine("Solving complete, result:");
+            Console.Write(Board.Print());
+
+            PressEnterToContinue();        
+        }
+
+        private static void PressEnterToContinue()
+        {
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
         }
     }
 }
