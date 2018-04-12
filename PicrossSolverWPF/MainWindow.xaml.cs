ca0bef80e47fe011051617477192c14734138141
rzippo <raffaele.zippo@live.it>
2018-04-12 09:06:32 +0200

    Implement board's ViewModel
    Need to figure out the Item/DataTemplate to continue

--- PicrossSolverWPF/MainWindow.xaml.cs
+++ PicrossSolverWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Newtonsoft.Json;
 using PicrossSolverLibrary;
 
 
@@ -23,7 +24,10 @@ namespace PicrossSolverWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public PicrossBoard PicrossBoard { get; set; }
+        public PicrossBoard Board { get; set; }
+        public PicrossPuzzle Puzzle { get; private set; }
+
+        public PicrossCellView[][] ViewMatrix { get; set; }
 
         public MainWindow()
         {
@@ -32,12 +36,70 @@ namespace PicrossSolverWPF
 
         private void JsonPathSelector_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Json files(*.json)|*.json",
+                Multiselect = false
+            };
+
+            bool? result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                JsonPath.Text = openFileDialog.FileName;
+            }
         }
 
         private void Load_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (FileStream fs = new FileStream(JsonPath.Text, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string json = sr.ReadToEnd();
+                        Puzzle = JsonConvert.DeserializeObject<PicrossPuzzle>(json);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error while opening json");
+            }
+
+            Board = new PicrossBoard(Puzzle);
+            LoadPicrossGrid();
+        }
+
+        private void LoadPicrossGrid()
+        {
+            BuildMatrix();
+
+            //todo: add visual representation
+            // https://stackoverflow.com/questions/25004713/creating-a-grid-filled-with-rectangles
+
+            MessageBox.Show("Ok");
+        }
+
+        private void BuildMatrix()
+        {
+            ViewMatrix = new PicrossCellView[Board.RowCount][];
+            for (int rowIndex = 0; rowIndex < Board.ColumnCount; rowIndex++)
+            {
+                var row = new PicrossCellView[Board.ColumnCount];
+                for (int columnIndex = 0; columnIndex < Board.RowCount; columnIndex++)
+                {
+                    row[columnIndex] = new PicrossCellView(Board.Matrix[rowIndex, columnIndex]);
+                }
+                ViewMatrix[rowIndex] = row;
+            }
+        }
+
+        private void Solve_Click(object sender, RoutedEventArgs e)
+        {
+            Board.DebugSolve();
+            MessageBox.Show("Ok");
         }
     }
 }
---
+++ PicrossSolverWPF/PicrossCellView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using PicrossSolverLibrary;
+
+namespace PicrossSolverWPF
+{
+    public class PicrossCellView   
+    {
+        public Rectangle Rectangle { get; set; }
+        public PicrossCell Cell { get; set; }
+
+        public PicrossCellView(PicrossCell observedCell)
+        {
+            Rectangle = new Rectangle()
+            {
+                Height = 5,
+                Width = 5,
+                Stroke = Brushes.Black,
+                StrokeThickness = 1
+            };
+
+            Cell = observedCell;
+            Cell.PropertyChanged += UpdateRectangle;
+        }
+
+        private void UpdateRectangle(object sender, PropertyChangedEventArgs e)
+        {
+            switch (Cell.State)
+            {
+                case PicrossCellState.Void:
+                    Rectangle.Fill = Brushes.White;
+                    break;
+                case PicrossCellState.Filled:
+                    Rectangle.Fill = Brushes.Green;
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
