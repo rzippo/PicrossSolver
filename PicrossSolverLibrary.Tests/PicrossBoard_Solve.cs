43c964ba349fbc26a890005d28b44ba93bc56938
rzippo <raffaele.zippo@live.it>
2018-05-07 23:05:52 +0200

    Add complete Solve test, for p050

---
+++ PicrossSolverLibrary.Tests/PicrossBoard_Solve.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace PicrossSolverLibrary.Tests
+{
+    public class PicrossBoard_Solve
+    {
+        [Fact]
+        public void LoadFromJson_Solve()
+        {
+            using (FileStream fs = new FileStream("p050.json", FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string json = sr.ReadToEnd();
+                    PicrossPuzzle puzzle = JsonConvert.DeserializeObject<PicrossPuzzle>(json);
+                    PicrossBoard board = new PicrossBoard(puzzle);
+                    board.Solve();
+                    Assert.True(board.IsSolved);
+                }
+            }
+        }
+    }
+}
